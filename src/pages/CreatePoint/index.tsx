85703a1b7722ada77795af6d09ad28c6d8d7a3e9
caiowf <caioweliton@alu.ufc.br>
2020-06-07 19:03:33 -0300

    add dropzone

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -8,6 +8,7 @@ import api from '../../services/api';
 import ibge from '../../services/ibge';
 import {LeafletMouseEvent} from 'leaflet';
 import Success from './components/Success';
+import Dropzone from '../../components/Dropzone';
 
 interface Item {
   id: number,
@@ -31,6 +32,7 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -111,17 +113,20 @@ const CreatePoint = () => {
     const city = selectedCity;
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
+
+    const data = new FormData();
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
     
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+    if(selectedFile) {
+      data.append('image', selectedFile);
+    }
     
     await api.post('points', data);
     
@@ -144,6 +149,7 @@ const CreatePoint = () => {
       </header>
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br/> ponto de coleta</h1>
+        <Dropzone onFileUploaded={setSelectedFile}/>
         <fieldset>
           <legend>
             <h2>Dados</h2>
